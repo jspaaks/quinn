58eb497dda04c994c66bf919ae765c5dd3f63d93
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-10-03 08:19:01 -0400

    reorganized the identifiers library for increased performance

--- 1120410/src/identifiers/identifiers.c
+++ 1120410/src/identifiers/identifiers.c
@@ -2,23 +2,19 @@
 #include "identifiers/identifiers.h"
 
 bool isvalid (char * identifier, size_t n) {
-    // rule 0: character should be in range '0' - '9'
+
+    // rule 0: characters at [0,..,n-1] should be in range '0' - '9', character at n should be '\0'
+    // rule 1: identifiers can't start with 0
+    // rule 2: consecutive digits can't be the same
+    // rule 3: identifiers can't sum to 7, 11, or 13
+
+    if (identifier[0] == '0') return false;
+
+    int summed = 0;
     for (size_t i = 0; i < n; i++) {
         if (identifier[i] < '0') return false;
         if (identifier[i] > '9') return false;
-    }
-
-    // rule 1: identifiers can't start with 0
-    if (identifier[0] == '0') return false;
-
-    // rule 2: consecutive digits can't be the same
-    for (size_t i = 1; i < n; i++) {
-        if (identifier[i-1] == identifier[i]) return false;
-    }
-
-    // rule 3: identifiers can't start sum to 7, 11, or 13
-    int summed = 0;
-    for (size_t i = 0; i < n; i++) {
+        if (identifier[i] == identifier[i+1]) return false;  // critically dependant on rule 0
         summed += identifier[i] - '0';
     }
 
