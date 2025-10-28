333c3ad362aeae82a5fdf17fd46c7fda7a2c5c83
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-10-28 08:10:46 -0400

    small refactor

--- 124/src/refactored/main.c
+++ 124/src/refactored/main.c
@@ -2,6 +2,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "blkdcmp/blkdcmp.h"
 
 int main (int argc, char *argv[]) {
@@ -13,12 +14,13 @@ int main (int argc, char *argv[]) {
     int    index;        // Index of current prime
     int    irank;        // This process's rank
     int    low_value;    // Lowest value on this process
-    bool * isnonprime;   // Portion of 2,...,`n`
+    bool  *isnonprime;   // Portion of 2,...,`n`
     int    n;            // Determine the number of primes that are smaller than this integer
     int    nranks;       // Number of processes
     int    prime;        // Current prime
-    int    nelems0;      // Size of proc0's subarray
     int    nelems;       // Elements in `isnonprime`
+
+    char msg[1001] = {};
 
     MPI_Init(&argc, &argv);
 
@@ -31,38 +33,29 @@ int main (int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
 
     if (argc != 2) {
-        if (irank == 0) {
-            fprintf(stderr, "Command line: %s N\n    Determine the number of primes in [2,...,N]\n", argv[0]);
-        }
-        MPI_Finalize();
-        return EXIT_FAILURE;
+        strncpy(msg, "Usage: sieve-refactored N\n    Determine the number of primes in [2,...,N]\n", 1000);
+        goto err;
     }
 
     n = atoi(argv[1]);
 
     // Figure out this process's share of the array, as well as the integers represented by the
-    // first and last elements
+    // first element of isnonprime in each process
     low_value = 2 + blkdcmp_get_idx_blk_s(n-1, nranks, irank);
     nelems = blkdcmp_get_blk_sz(n-1, nranks, irank);
 
     // Bail out if all the primes used for sieving are not all held by process 0
     high_value0 = 2 + blkdcmp_get_idx_blk_e(n-1, nranks, 0);
-    if (high_value0 < (int) sqrt((double) n)) {
-        if (irank == 0) {
-            fprintf(stderr, "Too many processes\n");
-        }
-        MPI_Finalize();
-        return EXIT_FAILURE;
+    if (high_value0 < (int) sqrt(n)) {
+        strncpy(msg, "Too many processes\n", 1000);
+        goto err;
     }
 
     // Allocate this process's share of the array
     isnonprime = (bool *) calloc(nelems, sizeof(bool));
     if (isnonprime == nullptr) {
-        if (irank == 0) {
-            fprintf(stderr, "Cannot allocate enough memory.\n");
-        }
-        MPI_Finalize();
-        return EXIT_FAILURE;
+        strncpy(msg, "Cannot allocate enough memory.\n", 1000);
+        goto err;
     }
 
     // Start marking elements of 'isnonprime' as nonprime until you get to the prime whose square is
@@ -114,4 +107,10 @@ int main (int argc, char *argv[]) {
 
     free(isnonprime);
     return EXIT_SUCCESS;
+err:
+    if (irank == 0) {
+        fprintf(stderr, "%s", msg);
+    }
+    MPI_Finalize();
+    return EXIT_FAILURE;
 }
