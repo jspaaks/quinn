1942ca22b665045a245f5a2dd4afd11ba26cd405
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-01 13:18:42 -0400

    corrected potential roundoff error in function val2ind; variable m now represents the number of elements, not the index of the last element

--- 1350506/src/blkdcmp-demo/main.c
+++ 1350506/src/blkdcmp-demo/main.c
@@ -5,7 +5,7 @@
 #include "blkdcmp/blkdcmp.h"
 
 int val2idx (int val) {
-    return (int) floor(((float) val - 3) / 2);
+    return (val - 3) / 2;  // truncate
 }
 
 int idx2val (int idx) {
@@ -24,31 +24,31 @@ int main (int argc, char *argv[]) {
     // get the user input
     int n = atoi(argv[1]);
     int nranks = atoi(argv[2]);
-    int m = val2idx(n);
+    int m = val2idx(n) + 1;
 
     if (n < 3) {
         fprintf(stderr, "N needs to be at least 3, aborting\n");
         return EXIT_FAILURE;
     }
 
-    if (nranks > m + 1) {
+    if (nranks > m) {
         fprintf(stderr, "Too many processes, aborting\n");
         return EXIT_FAILURE;
     }
 
-    fprintf(stdout, "idx      value    owner\n");
-    for (int idx = 0; idx <= m; idx++) {
+    fprintf(stdout, "idx        value      owner\n");
+    for (int idx = 0; idx < m; idx++) {
         int val = idx2val(idx);
-        int owner = (int) blkdcmp_get_blk_owner(m+1, nranks, idx);
-        fprintf(stdout, " %8d %8d %8d\n", idx, val, owner);
+        int owner = (int) blkdcmp_get_blk_owner(m, nranks, idx);
+        fprintf(stdout, " %10d %10d %10d\n", idx, val, owner);
     }
 
     fprintf(stdout, "---------------------------\n\n");
 
     for (int irank = 0; irank < nranks; irank++) {
-        int blk_s = blkdcmp_get_idx_blk_s(m+1, nranks, irank);
-        int blk_e = blkdcmp_get_idx_blk_e(m+1, nranks, irank);
-        int blk_sz = blkdcmp_get_blk_sz(m+1, nranks, irank);
+        int blk_s = blkdcmp_get_idx_blk_s(m, nranks, irank);
+        int blk_e = blkdcmp_get_idx_blk_e(m, nranks, irank);
+        int blk_sz = blkdcmp_get_blk_sz(m, nranks, irank);
         fprintf(stdout, "%d: %d..%d (%d)\n", irank, blk_s, blk_e, blk_sz);
     }
 
