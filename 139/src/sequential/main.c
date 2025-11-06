d42e703812497945e1514b64d69422409640cab0
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-06 16:21:51 -0500

    updated sequential solution for floyd's algorithm such that it can read binary data from an idx file, using third-party libidx dependency

--- 139/src/sequential/main.c
+++ 139/src/sequential/main.c
@@ -1,90 +1,79 @@
+#include <inttypes.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/param.h>
+#include "idx/idx.h"
 
-void calc_floyd (int n, int ** mat);
-void initialize_data(int n, const int * vec, int ** mat);
-void print_matrix(int n, const int ** mat);
+void calc_floyd (uint32_t n, uint8_t ** mat);
+void print_matrix(uint32_t n, const uint8_t ** mat);
+void show_help(const char * argv[]);
 
-static constexpr int basically_inf = INT_MAX / 2;
+static constexpr uint8_t basically_inf = 0x63;
 
-void calc_floyd (int n, int ** mat) {
-    for (int ivia = 0; ivia < n; ivia++) {
-        fprintf(stdout, "ivia=%d\n", ivia);
-        print_matrix(n, (const int **) mat);
-        for (int isrc = 0; isrc < n; isrc++) {
-            for (int idst = 0; idst < n; idst++) {
-                int direct = mat[isrc][idst];
-                int detour = mat[isrc][ivia] + mat[ivia][idst];
+void calc_floyd (uint32_t n, uint8_t ** mat) {
+    fprintf(stdout, "adjacency matrix:\n");
+    print_matrix(n, (const uint8_t **) mat);
+    fprintf(stdout, "\n\n");
+    for (uint32_t ivia = 0; ivia < n; ivia++) {
+        for (uint32_t isrc = 0; isrc < n; isrc++) {
+            for (uint32_t idst = 0; idst < n; idst++) {
+                uint8_t direct = mat[isrc][idst];
+                uint8_t detour = mat[isrc][ivia] + mat[ivia][idst];
                 mat[isrc][idst] = MIN(direct, detour);
             }
         }
+        fprintf(stdout, "shortest path length after ivia=%d%s:\n", ivia, ivia == n - 1 ? " (final)" : "");
+        print_matrix(n, (const uint8_t **) mat);
         fprintf(stdout, "\n\n");
     }
 }
 
-int main (void) {
-
-    int n = 6;
-
-    int * vec = (int *) calloc(n*n, sizeof(int));
-    if (vec == nullptr) {
-        fprintf(stderr, "Encountered problem during memory allocation for variable 'vec', aborting\n");
+int main (int argc, const char * argv[]) {
+    if (argc != 2) {
+        show_help(argv);
+        return EXIT_FAILURE;
     }
-    int ** mat = (int **) calloc(n, sizeof(int *));
+
+    const char * filepath = argv[1];
+    IdxHeader header = idx_read_header(filepath);
+    uint8_t * body = idx_read_body_as_uint8(filepath, &header);
+    uint8_t ** mat = calloc(header.nelems, sizeof(uint8_t *));
     if (mat == nullptr) {
         fprintf(stderr, "Encountered problem during memory allocation for variable 'mat', aborting\n");
     }
-    initialize_data(n, vec, mat);
-
+    uint32_t nrows = header.lengths[0];
+    uint32_t ncols = header.lengths[1];
+    for (uint32_t irow = 0; irow < nrows; irow++) {
+        mat[irow] = &body[irow * ncols];
+    }
 
     fprintf(stdout, "Example of Floyd's algorithm\n\n");
-    fprintf(stdout, "adjacency:\n");
-    print_matrix(n, (const int **) mat);
-    fprintf(stdout, "\n");
-
-    fprintf(stdout, "shortest path:\n");
-    calc_floyd(n, mat);
-    print_matrix(n, (const int **) mat);
-
+    calc_floyd(header.lengths[0], mat);
+    idx_free_body((void **) &body);
     return EXIT_SUCCESS;
+
 }
 
-void initialize_data(int n, const int * vec, int ** mat) {
-    for (int isrc = 0; isrc < n; isrc++) {
-        mat[isrc] = (int *) &vec[isrc * n];
-        for (int idst = 0; idst < n; idst++) {
-            if (isrc == idst) {
-                mat[isrc][idst] = 0;
-            } else {
-                mat[isrc][idst] = basically_inf;
-            }
-        }
-    }
-    mat[0][1] = 2;
-    mat[0][2] = 5;
-    mat[1][2] = 7;
-    mat[1][3] = 1;
-    mat[1][5] = 8;
-    mat[2][3] = 4;
-    mat[3][4] = 3;
-    mat[4][2] = 2;
-    mat[4][5] = 3;
-    mat[5][1] = 5;
-    mat[5][3] = 2;
-    mat[5][4] = 4;
-}
-
-void print_matrix(int n, const int ** mat) {
-    for (int isrc = 0; isrc < n; isrc++) {
-        for (int idst = 0; idst < n; idst++) {
+void print_matrix(uint32_t n, const uint8_t ** mat) {
+    for (uint32_t isrc = 0; isrc < n; isrc++) {
+        for (uint32_t idst = 0; idst < n; idst++) {
             if (mat[isrc][idst] == basically_inf) {
                 fprintf(stdout, "%5s ", "Inf");
             } else {
-                fprintf(stdout, "%5d ", mat[isrc][idst]);
+                fprintf(stdout, "%5" PRIu8 " ", mat[isrc][idst]);
             }
         }
         fprintf(stdout, "\n");
     }
+}
+
+
+void show_help (const char * argv[]) {
+    fprintf(stderr,
+            "Usage: %s FILEPATH\n"
+            "    Read a directed acyclic graph's adjacency matrix from FILEPATH and\n"
+            "    use Floyd's algorithm to determine the shortest-path matrix.\n"
+            , argv[0]);
 }
